49005770eb4fade2c54015d1e7e89a7be15fae5f
Daniel Lanthier <dlanthier@macadamian.com>
2015-06-18 12:03:18 -0400

    Removing the Actual plugin.
    Reason: it needs to be in its own repo. This is a repository for a sample application.

--- plugins/com.macadamian.blinkup/www/blinkup.js
+++
@@ -1,8 +0,0 @@
-/*global cordova, module*/
-
-module.exports = {
-    //API key is a string, timeoutMs is an int
-    invokeBlinkUp: function (apiKey, timeoutMs, useCachedPlanId, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "BlinkUpPlugin", "invokeBlinkUp", [apiKey, timeoutMs, useCachedPlanId]);
-    }
-};
